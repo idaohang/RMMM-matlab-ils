8caba68b3c0cd830161c3a549d785665d459740c
breen.machine <breen.machine@gmail.com>
2011-04-18 16:22:12 +0000

    Added draw graphs

---
+++ Results/newReductionStrategyTest/drawGraphs.m
@@ -0,0 +1,9 @@
+for i = 1:length(noises)
+    subplot(4,2,i);
+    semilogy(sizes,mean(squeeze(expand1(:,i,:))'),sizes,mean(squeeze(expand2(:,i,:))'));
+    legend('LLL','LLL+Permu');
+    xlabel('Problem Size');
+    ylabel('Nodes Expanded');
+    temp = strcat('Noise = ',num2str(noises(i)));
+    title(temp);
+end
