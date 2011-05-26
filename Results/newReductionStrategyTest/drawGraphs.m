8cbfc9665eecc39ec9e4345e6af1ec2f35c7b1d7
breen.machine <breen.machine@gmail.com>
2011-05-26 18:02:09 +0000

    added second test reduction large test

--- Results/newReductionStrategyTest/drawGraphs.m
+++ Results/newReductionStrategyTest/drawGraphs.m
@@ -1,6 +1,6 @@
 for i = 1:length(noises)
     subplot(4,2,i);
-    plot(sizes,mean(squeeze(expand1(:,i,:))'),sizes,mean(squeeze(expand2(:,i,:))'),sizes,mean(squeeze(expand3(:,i,:))'));
+    plot(sizes,mean(squeeze(expand1(1:4,i,:))'),sizes,mean(squeeze(expand2(1:4,i,:))'),sizes,mean(squeeze(expand3(1:4,i,:))'));
     legend('LLL','LLL+Permu','TestReduction','Location','NorthWest');
     xlabel('Problem Size');
     ylabel('Nodes Expanded');
