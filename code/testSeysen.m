815811f56be38782bd2e7d1370d7d7801b949e28
breen.machine <breen.machine@gmail.com>
2011-06-28 18:02:30 +0000

    theis coming

--- code/testSeysen.m
+++ code/testSeysen.m
@@ -1,5 +1,5 @@
-n = 40;
-sigma = 0.4;
+n = 50;
+sigma = 0.9;
 
 maxIter = 100;
 time1 = zeros(1,maxIter);
@@ -7,36 +7,62 @@ time2 = zeros(1,maxIter);
 condNum = zeros(1,maxIter);
 lb = -inf(n,1);
 ub = inf(n,1);
+babBetter = zeros(1,maxIter);
 
 for count = 1:maxIter
 count
-H = genMatrixAll(n,8);
+H = genMatrixAll(n,11);
 x = (-1*ones(n,1)).^(mod(round(rand(n,1)*10),2)+1).*round(rand(n,1)*10);
 snr = norm(H*x)^2/(n*sigma^2);
 v = sigma*randn(n,1);
 y = H*x + v;
-snr
 
 l = -inf*ones(n,1);
 u = inf*ones(n,1);
 [R2 Z2 y2] = reduction(H,y);
-%P = otherConstrainedReduction(R2,y2,lb,ub);
-%[Q3 R3] = qr(R2(:,P));
-%y3 = Q3'*y2;
-[R3 Z3 y3] = testReduction(H,y,snr^(0.5));
+P = otherConstrainedReduction(R2,y2,lb,ub);
+[Q3 R3] = qr(R2(:,P));
+y3 = Q3'*y2;
+%[R3 Z3 y3] = testReduction(H,y,snr^(0.5));
+
+[sorted,idx] = sort(P);
+babai1(count) = norm(H*Z2*babai(R2,y2) - y);
+if(norm(Z2*babai(R2,y2) - x) == 0)
+    babaiCorrect1(count) = 1;
+end
+
+babai2temp = babai(R3,y3);
+babai2(count) = norm(H*Z2*babai2temp(idx)-y);
+if(norm(Z2*babai2temp(idx) - x) == 0)
+    babaiCorrect2(count) = 1;
+end
+babai1(count)
+babai2(count)
+
 
 tic
 [z_ils] = mexsearch(R2,y2);
 t_z1 = toc
-
-tic;
-[z_ils2] = mexsearch(R3,y3);
-t_z2 = toc
-
 z_ils = Z2*z_ils;
-z_ils2 = Z3*z_ils2;
-%[~, idx] = sort(P);
-%z_ils2 = Z2*z_ils2(idx);
+
+
+if(babai2(count) < babai1(count))
+    babBetter(count) = 1;
+    tic;
+    [z_ils2] = mexsearch(R3,y3);
+    t_z2 = toc
+    [~, idx] = sort(P);
+    z_ils2 = Z2*z_ils2(idx);
+else
+    tic;
+    [z_ils2] = mexsearch(R2,y2);
+    t_z2 = toc 
+    z_ils2 = Z2*z_ils2;
+end
+
+
+%z_ils2 = Z3*z_ils2;
+
 
 if(norm(z_ils-z_ils2) ~= 0)
     error('Wrong Answer!');
--- test.m
+++ test.m
@@ -1,107 +1,97 @@
-runs = 500;
-time3 = zeros(1,runs);
-time1 = zeros(1,runs);
-time2 = zeros(1,runs);
-expand1 = zeros(1,runs);
-expand2 = zeros(1,runs);
-expand3 = zeros(1,runs);
-checkSum = zeros(1,runs);
-offDiagSum = zeros(1,runs);
-rowSum = zeros(1,runs);
-checkSum2 = zeros(1,runs);
-offDiagSum2 = zeros(1,runs);
-rowSum2 = zeros(1,runs);
-babai1 = zeros(1,runs);
-babai2 = zeros(1,runs);
-babai3 = zeros(1,runs);
-babaiCorrect1 = zeros(1,runs);
-babaiCorrect2 = zeros(1,runs);
-ilsCorrect1 = zeros(1,runs);
-ilsCorrect2 = zeros(1,runs);
-successRate1 = zeros(1,runs);
-successRate2 = zeros(1,runs);
-successRate3 = zeros(1,runs);
-sdevs = zeros(1,runs);
-snrs = zeros(1,runs);
-babaiBetter = zeros(1,runs);
-spikes = zeros(1,runs);
+n = 33;
+sigmas = [0.4];
 
+maxIter = 100;
+time1 = zeros(3,maxIter);
+time2 = zeros(3,maxIter);
+time3 = zeros(3,maxIter);
+time4 = zeros(3,maxIter);
+condNum = zeros(3,maxIter);
+lb = -inf(n,1);
+ub = inf(n,1);
+babBetter = zeros(3,maxIter);
 
-sz = 40;
-qam = 8;
-m = sz;
-n = m;
-sigma2 = 0.5;
-for i = 1:runs
-    i
-    %
-    % A small example to run function ils.m
-    %
-    % Construct data
-    B = genMatrixAll(sz,8);
-    z_true = (-1*ones(m,1)).^(mod(round(rand(m,1)*10),2)+1).*ceil(rand(m,1)*sqrt(qam));
-    lower = -10;
-    upper = 10;
-    sdevs(i) = sigma2;
-    noiseVec = sigma2*(0 + randn(m,1));
-    snrs(i) = norm(B*z_true)^2/(n*sigma2^2);
-    snrs(i)
-    y = B*z_true + noiseVec;
-    
-    l = -inf*(ones(1,n));
-    u = inf*(ones(1,n));
+outerCount=0;
+for sigma = sigmas
+    outerCount=outerCount+1;
+    for count = 1:maxIter
+    outerCount,count
+    H = genMatrixAll(n,10);
+    x = (-1*ones(n,1)).^(mod(round(rand(n,1)*10),2)+1).*round(rand(n,1)*10);
+    snr = norm(H*x)^2/(n*sigma^2);
+    v = sigma*randn(n,1);
+    y = H*x + v;
 
-    [R2 Z y2] = reduction(B,y);    
-    
-    tic;
-    [zhat,numExpanded] = search(R2,y2,1);
-    time1(i) = toc;
-    %numExpanded
-    expand1(i) = numExpanded;
-    successRate1(i) = successRate(R2,sigma2);
-    
-    [P z] = otherConstrainedReduction(R2,y2,l,u);
-    [Q R3] = qr(R2(:,P));
-    y3 = Q'*y2;
-    tic;
-    [zhat2,numExpanded2] = search(R3,y3,1);
-    time2(i) = toc;
-    %numExpanded2
-    expand2(i) = numExpanded2;
-    successRate2(i) = successRate(R3,sigma2);
-    [checkSum(i),rowSum(i),offDiagSum(i)] = checkLLL(R3);
+    l = -inf*ones(n,1);
+    u = inf*ones(n,1);
+    [R2 Z2 y2] = reduction(H,y);
+    P = otherConstrainedReduction(R2,y2,lb,ub);
+    [Q3 R3] = qr(R2(:,P));
+    y3 = Q3'*y2;
+    %[R3 Z3 y3] = testReduction(H,y,snr^(0.5));
+
     [sorted,idx] = sort(P);
-    
-    if(norm(zhat-zhat2(idx)) ~= 0)
-        error('Wrong answer!');
+    babai1(outerCount,count) = norm(H*Z2*babai(R2,y2) - y);
+    if(norm(Z2*babai(R2,y2) - x) == 0)
+        babaiCorrect1(outerCount,count) = 1;
     end
-    
-%     [R4 Z4 y4] = testReduction(B,y,snrs(i)^(0.5),sigma2);
-%     tic;
-%     [zhat3,numExpanded3] = search(R4,y4,1);
-%     time3(i) = toc;
-%     numExpanded3
-%     expand3(i) = numExpanded3;
-%     successRate3(i) = successRate(R4,sigma2);
-%     [checkSum2(i),rowSum2(i),offDiagSum2(i)] = checkLLL(R4);
-%     
-%     if(norm(Z*zhat-Z4*zhat3) ~= 0)
-%         error('Test Reduction Wrong Answer!');
-%     end
-%     
-    babai1(i) = norm(B*Z*babai(R2,y2) - y);
-    if(norm(Z*babai(R2,y2) - z_true) == 0)
-        babaiCorrect1(i) = 1;
-    end
-    
-    babai2temp = babai(R3,y3);
-    babai2(i) = norm(B*Z*babai2temp(idx)-y);
-    if(norm(Z*babai2temp(idx) - z_true) == 0)
-        babaiCorrect2(i) = 1;
-    end
-    babai1(i)
-    babai2(i)
 
-    
+    babai2temp = babai(R3,y3);
+    babai2(outerCount,count) = norm(H*Z2*babai2temp(idx)-y);
+    if(norm(Z2*babai2temp(idx) - x) == 0)
+        babaiCorrect2(outerCount,count) = 1;
+    end
+    babai1(outerCount,count)
+    babai2(outerCount,count)
+
+
+    tic;
+    [z_ils] = searchExtra(R2,y2,babai1(outerCount,count)^2+10^-6,0);
+    t_z1 = toc
+    z_ils = Z2*z_ils;
+
+    tic;
+        [z_ils4] = searchExtra(R3,y3,babai2(outerCount,count)^2+10^-6,0);
+    t_z4 = toc
+    [~, idx] = sort(P);
+    z_ils4 = Z2*z_ils4(idx);
+
+    if(babai2(outerCount,count) < babai1(outerCount,count))
+        babBetter(outerCount,count) = 1;
+        tic;
+        [z_ils2] = searchExtra(R2,y2,babai2(outerCount,count)^2+10^-6,0);
+        t_z2 = toc
+
+        tic;
+            [z_ils3] = searchExtra(R3,y3,babai2(outerCount,count)^2+10^-6,0);
+        t_z3 = toc
+        z_ils3 = Z2*z_ils3(idx);
+
+        z_ils2 = Z2*z_ils2;
+    else
+        tic;
+        [z_ils2] = searchExtra(R2,y2,babai1(outerCount,count)^2+10^-6,0);
+        t_z2 = toc
+        z_ils2 = Z2*z_ils2;
+
+        z_ils3 = z_ils2;
+        t_z3 = t_z2
+    end
+
+
+    %z_ils2 = Z3*z_ils2;
+
+
+    if(norm(z_ils-z_ils2) ~= 0 || norm(z_ils - z_ils3)~= 0 || norm(z_ils4 - z_ils) ~= 0 )
+        error('Wrong Answer!');
+    end
+
+    time1(outerCount,count) = t_z1;
+    time2(outerCount,count) = t_z2;
+    time3(outerCount,count) = t_z3;
+    time4(outerCount,count) = t_z4;
+
+    end
+
 end
 
